840fd60fc77a2e19de7cee69fa3a5b11a33e5853
sebas230704 <scanor2@eafit.edu.co>
2023-10-27 12:33:14 -0500

    cambios en estilos

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -17,8 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myapp import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render, get_object_or_404
+from django.db import IntegrityError
+from .forms import *
+from .models import *
+from datetime import datetime
+from django.conf import settings
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 
 # Create your views here.
 
@@ -10,3 +17,46 @@ def home(request):
     return render(request, 'home.html')
 
 
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'],
+                                                password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('home') #cambiar home a la pagina que tiene el base diferente
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    "error": 'Username already exists'
+                })
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            "error": 'Password do not match'
+        })
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+        'form' : AuthenticationForm
+    })
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+                'form' : AuthenticationForm,
+                'error' : 'Username or password is incorrect'
+            })
+            
+        else:
+            login(request, user)
+            return redirect('home')
